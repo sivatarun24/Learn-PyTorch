436778de058236f449ee8aedde8dcf7c9f0a66bd
Tarun <sivatarunereddy00@gmail.com>
2025-01-06 22:24:37 -0700

    created a ipynb file and added pytorch basics

--- pytorch_fundamentals.py
+++ pytorch_fundamentals.py
@@ -1,10 +1,10 @@
 import torch
 
-print(torch.__version__)
+# print(torch.__version__)
 tensor = torch.tensor([1, 2])
-print(tensor)
+print(tensor.ndim)
 
-torch.manual_seed(42)
-tensor_rand = torch.rand(2, 3)
-print(tensor_rand)
-print(tensor_rand.shape)
+# torch.manual_seed(42)
+# tensor_rand = torch.rand(2, 3)
+# print(tensor_rand)
+# print(tensor_rand.shape)
